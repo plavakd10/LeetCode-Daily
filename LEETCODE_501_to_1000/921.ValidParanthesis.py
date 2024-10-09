ae7be479947c41a1a1864ea885d3fc7bf8a59f0f
Plavak Das <plavakdas@gmail.com>
2024-10-09 20:36:05 +0530

    9th Oct

---
+++ LEETCODE_501_to_1000/921.ValidParanthesis.py
@@ -0,0 +1,10 @@
+def minAddToMakeValid(s) -> int:
+    st = []
+    for i in s:
+        if len(st)==0:
+            st.append(i)
+        elif st[-1]=='(' and i==')':
+            st.pop()
+        else:
+            st.append(i)
+    return len(st)
