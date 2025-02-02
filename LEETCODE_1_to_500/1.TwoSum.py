484637afedf93c93486cf9cef38bd017d953e881
Plavak Das <plavakdas@gmail.com>
2025-02-02 18:39:55 +0530

    two sum

--- LEETCODE_1_to_500/1.TwoSum.py
+++ LEETCODE_1_to_500/1.TwoSum.py
@@ -5,18 +5,3 @@ def two_sum(nums,target):
         if complement in dictionary:
             return [dictionary[complement],index]
         dictionary[value]=index
-
-
-
-
-nums = [2,7,11,15]
-target = 9
-print(two_sum(nums,target))   
-
-nums = [3,2,4]
-target = 6
-print(two_sum(nums,target))   
-
-nums = [3,3]
-target = 6
-print(two_sum(nums,target))   
