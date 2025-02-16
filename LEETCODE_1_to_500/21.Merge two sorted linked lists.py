16be87cd3eea542cdf648bdb42bd8f642f44bec8
Plavak Das <plavakdas@gmail.com>
2025-02-16 12:42:33 +0530

    21

---
+++ LEETCODE_1_to_500/21.Merge two sorted linked lists.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(list1,list2):
+        prehead = ListNode(-1)
+        prev = prehead
+        while list1 and list2:
+            if list1.val <= list2.val:
+                prev.next = list1
+                list1 = list1.next
+            else:
+                prev.next = list2
+                list2 = list2.next
+            prev = prev.next
+        prev.next = list1 if list1 is not None else list2
+        return prehead.next     
