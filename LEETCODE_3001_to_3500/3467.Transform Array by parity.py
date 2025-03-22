6a6bd3dcd4902afe7492076b1ba8b87773b57327
Plavak Das <plavakdas@gmail.com>
2025-03-22 20:26:03 +0530

    3467

---
+++ LEETCODE_3001_to_3500/3467.Transform Array by parity.py
@@ -0,0 +1,9 @@
+def transformArray(self, nums: List[int]) -> List[int]:
+    odd_count = 0
+    even_count = 0
+    for i in nums:
+        if i%2==0:
+            even_count+=1
+        else:
+            odd_count+=1
+    return ([0]*even_count)+([1]*odd_count)
