3ff204531b82c4ff39bb06c18e8c1df6dc661e83
Plavak Das <plavakdas@gmail.com>
2025-03-25 00:58:00 +0530

    1823

---
+++ LEETCODE_1501_to_2000/1823.Find the winner of the circular game.py
@@ -0,0 +1,11 @@
+from collections import deque
+def findTheWinner(n: int, k: int) -> int:
+    q = deque()
+    for i in range(1,n+1):
+        q.append(i)
+    while len(q)>1:
+        for i in range(k-1):
+            num = q.popleft()
+            q.append(num)
+        q.popleft()
+    return q[0] 
