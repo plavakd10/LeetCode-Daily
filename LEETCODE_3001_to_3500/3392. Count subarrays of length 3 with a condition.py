4b0209fdc2f2e689a66fe3d6a8e820b96a83c350
Plavak Das <plavakdas@gmail.com>
2025-04-27 12:17:16 +0530

    3392

---
+++ LEETCODE_3001_to_3500/3392. Count subarrays of length 3 with a condition.py
@@ -0,0 +1,6 @@
+def countSubarrays(self, nums: List[int]) -> int:
+    count = 0
+    for i in range(len(nums)-2):
+        if 2*(nums[i]+nums[i+2])==nums[i+1]:
+            count+=1
+    return count 
