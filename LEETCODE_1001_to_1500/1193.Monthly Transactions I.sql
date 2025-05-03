9c50188c633c01e88aff4d958c87818c46b7f7d3
Plavak Das <plavakdas@gmail.com>
2025-05-04 00:02:55 +0530

    1174 and 1193

---
+++ LEETCODE_1001_to_1500/1193.Monthly Transactions I.sql
@@ -0,0 +1,8 @@
+select left(trans_date,7) as month,
+country,
+count(id) as trans_count,
+sum(state='approved') as approved_count,
+sum(amount) as trans_total_amount,
+sum((state='approved')*amount) as approved_total_amount
+from transactions
+group by month, country
