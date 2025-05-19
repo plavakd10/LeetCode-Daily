ebabcec49cfe78fcbf497f65c1b15a0cdf72f97d
Plavak Das <plavakdas@gmail.com>
2025-05-19 22:36:57 +0530

    3024

---
+++ LEETCODE_3001_to_3500/3024.Type of Triangle.py
@@ -0,0 +1,9 @@
+def triangleType(nums) -> str:
+    nums.sort()
+    if nums[2]>=nums[0]+nums[1]:
+        return "none"
+    if nums[0]==nums[2]:
+        return "equilateral"
+    if nums[0]==nums[1] or nums[1]==nums[2]:
+        return "isosceles"
+    return "scalene"
