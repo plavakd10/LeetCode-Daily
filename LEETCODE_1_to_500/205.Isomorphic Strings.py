e5d3fa05b50c3b8875f7b6da765d0dca68ed6932
Plavak Das <plavakdas@gmail.com>
2025-06-05 01:44:31 +0530

    205

---
+++ LEETCODE_1_to_500/205.Isomorphic Strings.py
@@ -0,0 +1,10 @@
+def isIsomorphic(s,t) -> bool:
+    mapST = {}
+    mapTS = {}
+    for i in range(len(s)):
+        c1,c2 = s[i],t[i]
+        if (c1 in mapST and mapST[c1]!=c2) or (c2 in mapTS and mapTS[c2]!=c1):
+            return False
+        mapST[c1]=c2
+        mapTS[c2]=c1
+    return True    
