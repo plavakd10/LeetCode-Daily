abe8178260ed357cc94a092d3918496dcefa0989
Plavak Das <plavakdas@gmail.com>
2025-06-06 19:36:13 +0530

    485

---
+++ LEETCODE_1_to_500/485.Max Consecutive Ones.py
@@ -0,0 +1,10 @@
+def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+    m = 0
+    c = 0
+    for i in range(len(nums)):
+        if nums[i]==1:
+            c+=1
+        else:
+            c=0
+        m = max(m,c)
+    return m            
