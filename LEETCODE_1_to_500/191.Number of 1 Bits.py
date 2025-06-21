a8639b87a5c3b28644b1cef80114189030ff4eaf
Plavak Das <plavakdas@gmail.com>
2025-06-21 22:51:33 +0530

    191

--- LEETCODE_1_to_500/191.Number of 1 Bits.py
+++ LEETCODE_1_to_500/191.Number of 1 Bits.py
@@ -1,2 +1,6 @@
-def hammingWeight(n: int) -> int:
-    return n.bit_count()
+def hammingWeight(self, n: int) -> int:
+    cnt = 0
+    while n!=0:
+        n = n & (n-1)
+        cnt+=1
+    return cnt    
