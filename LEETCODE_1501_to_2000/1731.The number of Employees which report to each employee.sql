820297fd9350247bf7636c6fbca9d5b2eb1228b4
Plavak Das <plavakdas@gmail.com>
2025-09-11 16:59:23 +0530

    1731

---
+++ LEETCODE_1501_to_2000/1731.The number of Employees which report to each employee.sql
@@ -0,0 +1,5 @@
+select mgr.employee_id, mgr.name, count(emp.employee_id) as reports_count, round(avg(emp.age)) as average_age
+from employees emp join employees mgr
+on emp.reports_to = mgr.employee_id
+group by employee_id
+order by employee_id
