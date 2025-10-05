a1a20c2df46f55b0e50fd90b1e7e9becb837b0c3
Plavak Das <plavakdas@gmail.com>
2025-10-05 15:30:13 +0530

    417

---
+++ LEETCODE_1_to_500/417.Pacific Atlantic Water flow.py
@@ -0,0 +1,24 @@
+def pacificAtlantic(heights):
+    rows, cols = len(heights), len(heights[0])
+    pac, atl = set(), set()
+
+    def dfs(r,c,visit,prevHeight):
+        if ((r,c) in visit or r<0 or c<0 or r==rows or c==cols or heights[r][c] < prevHeight):
+            return
+        visit.add((r,c))
+        dfs(r+1,c, visit,heights[r][c])
+        dfs(r-1,c, visit, heights[r][c])
+        dfs(r,c+1,visit,heights[r][c])
+        dfs(r,c-1,visit,heights[r][c])
+    for c in range(cols):
+        dfs(0,c,pac,heights[0][c])
+        dfs(rows-1,c,atl,heights[rows-1][c])
+    for r in range(rows):
+        dfs(r,0,pac,heights[r][0])
+        dfs(r,cols-1,atl,heights[r][cols-1])
+    ans = []
+    for r in range(rows):
+        for c in range(cols):
+            if (r,c) in pac and (r,c) in atl:
+                ans.append([r,c])
+    return ans
