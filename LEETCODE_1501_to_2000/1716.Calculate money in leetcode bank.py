119d944acf9ba02785314ab5d7e0c538a95c710a
Plavak Das <plavakdas@gmail.com>
2025-10-25 13:51:42 +0530

    1716

---
+++ LEETCODE_1501_to_2000/1716.Calculate money in leetcode bank.py
@@ -0,0 +1,9 @@
+def totalMoney(n) -> int:
+    ans = 0
+    monday = 1
+    while n>0:
+        for day in range(min(n,7)):
+            ans += monday+day
+        n-=7
+        monday+=1
+    return ans
