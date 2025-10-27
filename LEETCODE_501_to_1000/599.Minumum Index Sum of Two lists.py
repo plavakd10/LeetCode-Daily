6972758e383d04192344ba2dea0ed5ea83df3431
Plavak Das <plavakdas@gmail.com>
2025-10-28 00:07:29 +0530

    599

---
+++ LEETCODE_501_to_1000/599.Minumum Index Sum of Two lists.py
@@ -0,0 +1,15 @@
+from collections import Counter
+def findRestaurant(list1,list2) -> List[str]:
+    set1 = set(list1)
+    set2 = set(list2)
+
+    c = Counter(set1.intersection(set2))
+    min_dict = {} 
+    ans = []
+    for k,v in c.items():
+        min_dict[k] = list1.index(k) + list2.index(k)
+    m = min(min_dict.values())
+    for k,v in min_dict.items():
+        if v==m:
+            ans.append(k)
+    return ans 
