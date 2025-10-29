976d581555bc69038983ab66cd702cbd6141a28e
Plavak Das <plavakdas@gmail.com>
2025-10-30 02:36:23 +0530

    2677

---
+++ LEETCODE_2501_to_3000/2677.Chunk Array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    const res = [];
+    for (let i=0; i<arr.length;i+=size) {
+        res.push(arr.slice(i,i+size));
+    }
+
+    return res
+};
